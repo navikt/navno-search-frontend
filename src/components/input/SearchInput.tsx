2c3154c859b5ab51954c2b6189ae39a03b6017b6
anders <anders@nomerstad.com>
2020-10-22 10:20:26 +0200

    Setter max-lengde på søket

--- src/components/input/SearchInput.tsx
+++ src/components/input/SearchInput.tsx
@@ -4,6 +4,8 @@ import { BEM } from '../../utils/bem';
 import { Hovedknapp } from 'nav-frontend-knapper';
 import './SearchInput.less';
 
+const maxInputLength = 200;
+
 type Props = {
     initialSearchTerm: string;
     setSearchTerm: (term: string) => void;
@@ -31,7 +33,8 @@ export const SearchInput = ({
                 aria-labelledby={'search-header'}
                 className={bem('input')}
                 onChange={(e) => setSearchTerm(e.target.value)}
-                defaultValue={initialSearchTerm}
+                defaultValue={initialSearchTerm.slice(0, maxInputLength)}
+                maxLength={maxInputLength}
                 placeholder={'Søk på nav.no'}
             />
             <Hovedknapp className={bem('button')} htmlType={'submit'}>
