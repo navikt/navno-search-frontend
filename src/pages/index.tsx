78b0ede8ae97c4e937c9d8bfb368ac72b2de351e
anders-nom <anders@nomerstad.com>
2021-10-18 09:27:02 +0200

    Fjerner gammelt wip-varsel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,10 @@ import { GetServerSideProps } from 'next';
 import { fetchSearchResults } from '../utils/fetch-search-result';
 import { SearchResultProps } from '../types/search-result';
 import SearchPage from '../components/SearchPage';
-import { UndertekstBold } from 'nav-frontend-typografi';
 import { paramsFromResult } from '../types/search-params';
 import AlertStripe from 'nav-frontend-alertstriper';
 import Head from 'next/head';
 import { ContextProvider } from '../context/ContextProvider';
-import Config from '../config';
 import '../global-style.less';
 
 type Props = {
@@ -28,16 +26,6 @@ const SearchBase = (props: Props) => {
                     <title>{'Søk - nav.no'}</title>
                 </Head>
                 <div className={'content-wrapper'} id={'maincontent'}>
-                    {process.env.APP_BASE_PATH !==
-                        Config.PATHS.appBasePathProd && (
-                        <div className={'work-in-progress'}>
-                            <UndertekstBold>
-                                {
-                                    'Denne versjonen av søk på nav.no er under utvikling.'
-                                }
-                            </UndertekstBold>
-                        </div>
-                    )}
                     {initialResult ? (
                         <SearchPage />
                     ) : (
