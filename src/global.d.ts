85ab1fe87261520ff6c7e1a0db17f243a041716a
Anders Bråthen Nomerstad <anders@nomerstad.com>
2023-04-12 13:57:55 +0200

    Tar i bruk nav-dekoratoren-moduler for fetch av dekoratøren

---
+++ src/global.d.ts
@@ -0,0 +1,14 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            ENV: 'prod' | 'dev1' | 'dev2' | 'localhost';
+            APP_BASE_PATH: string;
+            APP_ORIGIN: string;
+            DECORATOR_LOCAL_URL: string;
+            XP_ORIGIN: string;
+            NODE_ENV: 'development' | 'production';
+        }
+    }
+}
+
+export {};
