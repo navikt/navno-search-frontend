b42afc7599cdfb22577d8864da2edab8ed5a99e5
Bjørn Harbo Dahle <bdahle@hey.com>
2023-12-05 11:53:47 +0100

    rename header to description

--- src/components/header/SearchDescription.tsx
+++ src/components/header/SearchDescription.tsx
@@ -6,7 +6,7 @@ import { BodyLong, BodyShort, Button } from '@navikt/ds-react';
 import Config from '../../config';
 import { paramToDaterangeKey } from '../../types/search-params';
 
-import style from './SearchHeader.module.scss';
+import style from './SearchDescription.module.scss';
 
 const { keys } = Config.VARS;
 
@@ -14,7 +14,7 @@ type Props = {
     result: SearchResultProps;
 };
 
-export const SearchHeader = ({ result }: Props) => {
+export const SearchDescription = ({ result }: Props) => {
     const [{ params }, dispatch] = useSearchContext();
 
     const selectedFacet = result.aggregations.fasetter.buckets.find(
@@ -34,7 +34,7 @@ export const SearchHeader = ({ result }: Props) => {
         !isDefaultDaterange;
 
     return (
-        <div className={style.searchHeader} id={'search-header'}>
+        <div className={style.searchDescription} id={'search-header'}>
             {hasSelectedNonDefaultFilters && (
                 <BodyLong>
                     {ufNames.length > 0 && (
