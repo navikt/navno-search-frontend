2dfc99790970849a61f89a2123ebc78328c47d07
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-03-12 15:31:00 +0100

    Litt cleanup

--- src/utils/decoratorCookies.ts
+++ src/utils/decoratorCookies.ts
@@ -1,23 +1,25 @@
 import { NextApiRequestCookies } from 'next/dist/server/api-utils';
 
-const validAudiences: { [key: string]: string } = {
-    privatperson: '0',
-    arbeidsgiver: '1',
-    samarbeidspartner: '2',
-};
+const validAudiences: ReadonlySet<string> = new Set([
+    'privatperson',
+    'arbeidsgiver',
+    'samarbeidspartner',
+]);
 
-const validLanguages = ['nb', 'nn', 'en'];
+const validLanguages: ReadonlySet<string> = new Set(['nb', 'nn', 'en']);
 
 export const getDecoratorAudienceIfValid = (cookies: NextApiRequestCookies) => {
     const decoratorAudience = cookies['decorator-context'];
 
-    return decoratorAudience ? validAudiences[decoratorAudience] : undefined;
+    return decoratorAudience && validAudiences.has(decoratorAudience)
+        ? decoratorAudience
+        : undefined;
 };
 
 export function getDecoratorLanguageIfValid(cookies: NextApiRequestCookies) {
     const decoratorLanguage = cookies['decorator-language'];
 
-    return decoratorLanguage && validLanguages.includes(decoratorLanguage)
+    return decoratorLanguage && validLanguages.has(decoratorLanguage)
         ? decoratorLanguage
         : undefined;
 }
